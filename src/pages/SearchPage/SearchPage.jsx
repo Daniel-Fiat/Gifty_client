604a68475bbf7748caf23db6bf24d37532348f51
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2022-12-07 13:28:55 +0100

    Search with word key.

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -3,7 +3,9 @@ import { useState, useEffect } from 'react';
 import ProductAPI from '../../services/product.service'
 const Search = () => {
     localStorage.setItem("Navbar", true);
-    const [products, setProducts] = useState([])
+    const [products, setProducts] = useState([]);
+    const [filter, setFilter] = useState();
+
 
     useEffect(() => {
         ProductAPI.getAllproduct().then(products => {
@@ -11,22 +13,34 @@ const Search = () => {
         })
     }, [])
 
+    const filterProducts = (event) => {
+        const { value } = event.target;
+        let _products = [...products];
+
+        _products = _products.filter((product) => product.name.toLowerCase().includes(value.toLowerCase()));
+
+        value ? setFilter(_products) : setFilter(undefined)
+    }
+
     return (
         <>
             <h1>Search</h1>
             <input className='SearchInput'
-                // onChange={updateUser}
+                onChange={filterProducts}
                 type='text'
                 name='SearchInput'
                 placeholder='Search'>
             </input>
             {
-                products.map(product => <h1>{product.name}</h1>)
+                filter
+                    ?
+                    filter.map(filter => <h1>{filter.name}</h1>)
+                    :
+                    <div><h1>Hola</h1></div>
             }
 
         </>
     );
-
 }
 
 export default Search;
