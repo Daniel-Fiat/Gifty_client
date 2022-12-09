eec24ddb678831fbee54d8c59655a72b959f961a
Daniel-Fiat <danielfiatcasado@gmail.com>
2022-12-09 14:13:36 +0100

    Register Login Page

--- src/pages/RegisterLoginPage/RegisterLoginPage.jsx
+++ src/pages/RegisterLoginPage/RegisterLoginPage.jsx
@@ -3,41 +3,10 @@ import elipseBlueUp from '../../assets/ElipseAzulArriba.png';
 import elipsePinkUp from '../../assets/ElipseRosaArriba.png';
 import elipseBlueDown from '../../assets/ElipseAzulAbajo.png';
 import elipseYellowDown from '../../assets/ElipseAmarillaAbajo.png';
-import UserApi from '../../services/user.service'
-import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     localStorage.removeItem("Navbar");
-    const navigate = useNavigate();
-    const [user, setUser] = useState({})
-    const [matchPass, setMatchPass] = useState(false)
-    const [errUserCreate, setError] = useState(false)
-
-    const createNewUSer = (event) => {
-        event.preventDefault()
-        if (matchPass && user.email) {
-            console.log(user)
-            UserApi.createUser(user)
-                .then(() => navigate('/'))
-                .catch(err => setError(true))
-        } else {
-            setError(true)
-        }
-    }
-    const updateNewUser = (event) => {
-        const { name, value } = event.target
-        setUser({ ...user, [name]: value });
-        console.log(user)
-    }
-    const testmatchPass = (event) => {
-        const { value } = event.target
-        if (value === user?.password) { setMatchPass(true) }
-        else { setMatchPass(false) }
-        console.log(value)
-        console.log(user?.password)
-    }
-
 
     return (
         <div className='register-form-container'>
